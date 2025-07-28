a512e46dfbc3421dca515f59e0d7448975554a2d
terist999 <xscammer25@gmail.com>
2025-07-28 13:55:34 +0800

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/as1An286PnXVBHNd/raw", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/MainLoader.lua"))()
+loadstring(game:HttpGet("https://pastefy.app/FXCqXRDH/raw"))()
